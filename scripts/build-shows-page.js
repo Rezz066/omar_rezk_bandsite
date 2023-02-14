5996c3b9bbb2bf776b7c0e91914f1a10245098f4
Omar Rezk <omar.rizk6@gmail.com>
2023-02-13 22:50:34 -0500

    created the form and the event dates

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,8 @@
+let objCache = [
+    {date: "Mon Sept 06 2021", venue: "Ronald Lane", location: "San Francisco, CA"},
+    {date: "Tues Sept 21 2021", venue: "Pier 3 East", location: "San Francisco, CA"},
+    {date: "Fri Oct 15 2021", venue: "View Lounge", location: "San Francisco, CA"},
+    {date: "Sat Nov 06 2021", venue: "Hyatt Agency", location: "San Francisco, CA"},
+    {date: "Fri Nov 26 2021 ", venue: "Moscow Center", location: "San Francisco, CA"},
+    {date: "Wed Dec 15 2021", venue: "Press Club", location: "San Francisco, CA"}
+]
