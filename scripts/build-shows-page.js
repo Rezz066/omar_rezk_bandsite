c15510081685e898a83f6af4014e77ddab37d22e
Omar Rezk <omar.rizk6@gmail.com>
2023-02-25 18:16:42 -0500

    added axios for the shows section in the shows page

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,40 +1,30 @@
-// shows data added into an array
-const showsData = [
-    {
-        date: "Mon Sept 06 2021",
-        venue: "Ronald Lane",
-        location: "San Francisco, CA"
-    },
-    {
-        date: "Tues Sept 21 2021",
-        venue: "Pier 3 East",
-        location: "San Francisco, CA"
-    },
-    {
-        date: "Fri Oct 15 2021",
-        venue: "View Lounge",
-        location: "San Francisco, CA"
-    },
-    {
-        date: "Sat Nov 06 2021",
-        venue: "Hyatt Agency",
-        location: "San Francisco, CA"
-    },
-    {
-        date: "Fri Nov 26 2021 ",
-        venue: "Moscow Center",
-        location: "San Francisco, CA"
-    },
-    {
-        date: "Wed Dec 15 2021",
-        venue: "Press Club",
-        location: "San Francisco, CA"
-    }
-]
 
 // The Skeleton of the show container, event dates and location
 
 const section = document.querySelector('.shows')
+
+const API_URL = "https://project-1-api.herokuapp.com/showdates"
+const API_KEY = "6f964ab1-8dc4-461d-b82a-fa87241c5278"
+
+
+const url = API_URL + `?api_key=${API_KEY}`
+
+function getShows(){
+
+axios.get(url)
+        .then((response) => {
+
+                const showsEl = response.data
+
+                showsEl.forEach((ele) => {
+                    showEvents (ele)
+                })
+            })
+
+
+}
+
+getShows()
 
         const sectionContainer = document.createElement('div')
         sectionContainer.classList.add('shows__titleContainer')
@@ -56,85 +46,84 @@ const section = document.querySelector('.shows')
         sectionContainer.appendChild(showsTitleLocation)
 
 
-// iterate through a for loop and log data with index [i]
-    for(let i =0; i< showsData.length; i++) {
-        const events = showsData[i]
-        console.log(events)
-
-        const showsContentEl = document.createElement('div')
-        showsContentEl.classList.add('shows__content')
-        section.appendChild(showsContentEl)
-
-        const showsContainerEl = document.createElement('div')
-        showsContainerEl.classList.add('shows__container')
-        showsContentEl.appendChild(showsContainerEl)
-
-        //create Shows Events inside parent (Shows Container)
-        const showsEventEl = document.createElement('div')
-        showsEventEl.classList.add('shows__event')
-        showsEventEl.classList.add('shows__event--link')
-        showsContainerEl.appendChild(showsEventEl)
-
-        //create Shows Box inside parent (Shows Event)
-        const showsBoxEl = document.createElement('div')
-        showsBoxEl.classList.add('shows__box')
-        showsEventEl.appendChild(showsBoxEl)
-
-        //create Shows Subtitle inside parent (Shows Box)
-        const showsSubtitleEl = document.createElement('h4')
-        showsSubtitleEl.classList.add('shows__subtitle')
-        showsSubtitleEl.innerText = "DATE";
-        showsBoxEl.appendChild(showsSubtitleEl)
-
-        //create Shows Details inside parent (Shows Box)
-        const showsDateDetailsEl = document.createElement('p')
-        showsDateDetailsEl.classList.add('shows__details')
-        showsDateDetailsEl.classList.add('shows__details--date')
-        showsDateDetailsEl.innerText = events.date;
-        showsBoxEl.appendChild(showsDateDetailsEl)
-
-        const showsBoxesEl = document.createElement('div')
-        showsBoxesEl.classList.add('shows__box')
-        showsEventEl.appendChild(showsBoxesEl)
-        
-        //create Shows Subtitle inside parent (Shows Box)
-        const showsVenueEl = document.createElement('h4')
-        showsVenueEl.classList.add('shows__subtitle')
-        showsVenueEl.innerText = "VENUE";
-        showsBoxesEl.appendChild(showsVenueEl)
-
-        //create Shows Details inside parent (Shows Box)
-        const showsVenueDetailsEl = document.createElement('p')
-        showsVenueDetailsEl.classList.add('shows__details')
-        showsVenueDetailsEl.innerText = events.venue;
-        showsBoxesEl.appendChild(showsVenueDetailsEl)
-
-        const showBoxesEl = document.createElement('div')
-        showBoxesEl.classList.add('shows__box')
-        showsEventEl.appendChild(showBoxesEl)
-
-        //create Shows Subtitle inside parent (Shows Box)
-        const showsLocationEl = document.createElement('h4')
-        showsLocationEl.classList.add('shows__subtitle')
-        showsLocationEl.innerText = "LOCATION";
-        showBoxesEl.appendChild(showsLocationEl)
-
-        //create Shows Details inside parent (Shows Box)
-        const showsLocationDetailsEl = document.createElement('p')
-        showsLocationDetailsEl.classList.add('shows__details')
-        showsLocationDetailsEl.innerText = events.location;
-        showBoxesEl.appendChild(showsLocationDetailsEl)
-
-        //create Shows Button box inside parent (Show Box)
-        const showsButtonEl = document.createElement('a')
-        showsButtonEl.classList.add('shows__button')
-        showsEventEl.appendChild(showsButtonEl)
-
-        //create Shows Button Link inside parent (Shows Button)
-        const showsButtonLinkEl = document.createElement('p')
-        showsButtonLinkEl.classList.add('shows__text')
-        showsButtonLinkEl.innerText = "BUY TICKETS"
-        showsButtonEl.appendChild(showsButtonLinkEl)
-    }
-
+function showEvents (ele){
     
+    const showsContentEl = document.createElement('div')
+    showsContentEl.classList.add('shows__content')
+    section.appendChild(showsContentEl)
+
+    const showsContainerEl = document.createElement('div')
+    showsContainerEl.classList.add('shows__container')
+    showsContentEl.appendChild(showsContainerEl)
+
+    //create Shows Events inside parent (Shows Container)
+    const showsEventEl = document.createElement('div')
+    showsEventEl.classList.add('shows__event')
+    showsEventEl.classList.add('shows__event--link')
+    showsContainerEl.appendChild(showsEventEl)
+
+    //create Shows Box inside parent (Shows Event)
+    const showsBoxEl = document.createElement('div')
+    showsBoxEl.classList.add('shows__box')
+    showsEventEl.appendChild(showsBoxEl)
+
+    //create Shows Subtitle inside parent (Shows Box)
+    const showsSubtitleEl = document.createElement('h4')
+    showsSubtitleEl.classList.add('shows__subtitle')
+    showsSubtitleEl.innerText = "DATE";
+    showsBoxEl.appendChild(showsSubtitleEl)
+
+    //create Shows Details inside parent (Shows Box)
+    const showsDateDetailsEl = document.createElement('p')
+    showsDateDetailsEl.classList.add('shows__details')
+    showsDateDetailsEl.classList.add('shows__details--date')
+    const formattedDate = new Date(ele.date);
+    const eventDate = formattedDate.toDateString()
+    showsDateDetailsEl.innerText = eventDate;
+    showsBoxEl.appendChild(showsDateDetailsEl)
+
+    const showsBoxesEl = document.createElement('div')
+    showsBoxesEl.classList.add('shows__box')
+    showsEventEl.appendChild(showsBoxesEl)
+    
+    //create Shows Subtitle inside parent (Shows Box)
+    const showsVenueEl = document.createElement('h4')
+    showsVenueEl.classList.add('shows__subtitle')
+    showsVenueEl.innerText = "VENUE";
+    showsBoxesEl.appendChild(showsVenueEl)
+
+    //create Shows Details inside parent (Shows Box)
+    const showsVenueDetailsEl = document.createElement('p')
+    showsVenueDetailsEl.classList.add('shows__details')
+    showsVenueDetailsEl.innerText = ele.place;
+    showsBoxesEl.appendChild(showsVenueDetailsEl)
+
+    const showBoxesEl = document.createElement('div')
+    showBoxesEl.classList.add('shows__box')
+    showsEventEl.appendChild(showBoxesEl)
+
+    //create Shows Subtitle inside parent (Shows Box)
+    const showsLocationEl = document.createElement('h4')
+    showsLocationEl.classList.add('shows__subtitle')
+    showsLocationEl.innerText = "LOCATION";
+    showBoxesEl.appendChild(showsLocationEl)
+
+    //create Shows Details inside parent (Shows Box)
+    const showsLocationDetailsEl = document.createElement('p')
+    showsLocationDetailsEl.classList.add('shows__details')
+    showsLocationDetailsEl.innerText = ele.location;
+    showBoxesEl.appendChild(showsLocationDetailsEl)
+
+    //create Shows Button box inside parent (Show Box)
+    const showsButtonEl = document.createElement('a')
+    showsButtonEl.classList.add('shows__button')
+    showsEventEl.appendChild(showsButtonEl)
+
+    //create Shows Button Link inside parent (Shows Button)
+    const showsButtonLinkEl = document.createElement('p')
+    showsButtonLinkEl.classList.add('shows__text')
+    showsButtonLinkEl.innerText = "BUY TICKETS"
+    showsButtonEl.appendChild(showsButtonLinkEl)
+
+}
+       
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,22 +1,3 @@
-// const commentsList = [
-//     {
-//         name: "Connor Walton",
-//         comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-//         date: "02/17/2021"
-
-//     },
-//     {
-//         name: "Emilie Beach",
-//         comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-//         date: "01/09/2020"
-//     },
-//     {
-//         name: "Miles Acosta",
-//         comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-//         date: "12/20/2020"
-//     }
-
-// ]
 
 
 const formEl = document.querySelector('.form')
@@ -35,8 +16,7 @@ function getComments(){
 
 axios
 .get(url)
-.then( (response) => {
-    console.log(response)
+.then((response) => {
     clearContainer()
 
     const dataEl = response.data.sort((a,b) => {
@@ -75,7 +55,6 @@ formEl.addEventListener('submit', (event) => {
             comment: comments
         })
         .then((response) => {
-            console.log(response)
             getComments()})
         .catch(error => console.log(error))
 
@@ -126,7 +105,6 @@ function renderComments (commentItems) {
         const formattedDate = new Date(commentItems.timestamp);
         console.log(formattedDate);
         const finalDate = (formattedDate.getMonth() + 1) + '/' + formattedDate.getDate()  + '/' + formattedDate.getFullYear();
-        console.log(finalDate);
         dateEl.innerText = finalDate
         commentsHeaderEl.appendChild(dateEl)
 
@@ -138,6 +116,4 @@ function renderComments (commentItems) {
    
 
 }
-
-// renderComments()
 
